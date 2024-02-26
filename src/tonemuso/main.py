6c28a44c3b90528953a4160d4acb94fe9eeea01a
Andrey Tvorozhkov <andrey@head-labs.com>
2024-02-26 17:35:48 +0300

    ????

--- src/tonemuso/main.py
+++ src/tonemuso/main.py
@@ -25,17 +25,27 @@ def get_diff(tx1, tx2):
     return diff, address
 
 
+def lisp_length(lst):
+    if not lst:  # if the list is empty
+        return 0
+    elif isinstance(lst, (tuple, list)):
+        return 1 + sum(lisp_length(item) for item in lst)
+    else:
+        return 1
+
+
 def process_blocks(data):
     out = []
     block, account_state, txs = data
     config = block['key_block']['config']
 
     em = EmulatorExtern(os.getenv("EMULATOR_PATH"), config)
+    print(lisp_length(block['prev_block_data']))
 
     em.set_rand_seed(block['rand_seed'])
     block['prev_block_data'][0] = list(reversed(block['prev_block_data']))
     try:
-        print(len(block['prev_block_data']), len(block['prev_block_data'][0]))
+
         em.set_prev_blocks_info(block['prev_block_data'])
     except Exception as e:
         print(e, block['prev_block_data'])
